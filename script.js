7209a0f360e0adb88bb1c7f05a36458e96dedb38
wfceza <73784410+wfceza@users.noreply.github.com>
2023-11-02 10:08:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,23 @@ mobileBtn.addEventListener('click',  () => {
 mobileBtnExit.addEventListener('click', ()  =>  {
     nav.classList.remove('menu-btn')
 })
+
+window.addEventListener('scroll', reveal);
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active');
+        }
+        else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
